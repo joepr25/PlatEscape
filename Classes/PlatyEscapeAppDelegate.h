5f0e11ff735a52dcbe54b4e243b22aa3dd27301d
Jose Rodrigo Perez <joepr25@gmail.com>
2011-07-07 14:09:37 -0500

    Commit Inicial // Esta será la versión limpia que se subirá a la appstore. 	*	Se añadió el .gitignore

---
+++ Classes/PlatyEscapeAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PlatyEscapeAppDelegate.h
+//  PlatyEscape
+//
+//  Created by José Rodrigo Pérez Rodríguez on 7/7/11.
+//  Copyright 2011 Itesm Campus Puebla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlatyEscapeAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
